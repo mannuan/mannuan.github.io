984e7798cf44e998bcff210d9a41aca1ab502c00
wjl <1271990125@qq.com>
2019-08-20 21:51:57 +0800

    '个人技能4'

--- knowledge-architecture/vue.config.js
+++
@@ -1,9 +0,0 @@
-const devPublicPath = "/";
-const proPublicPath = "/knowledge-architecture/dist/";
-let publicPath;
-process.env.NODE_ENV === "development"
-  ? (publicPath = devPublicPath)
-  : (publicPath = proPublicPath);
-module.exports = {
-  publicPath: publicPath
-};
