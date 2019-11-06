982afa7aa0824fd2413c9f0b60b1fc9c78df8498
wjl <1271990125@qq.com>
2019-11-06 10:23:35 +0800

    ''

--- tests/c&c++/test.cpp
+++ tests/c&c++/test.cpp
@@ -1,25 +1,105 @@
-#include<iostream>
+# include<cstdio>
+# include<iostream>
+# include<cstring>
 using namespace std;
-int main(){
-    int n,max = 9;
-    cin>>n;
-    if(n<10){
-        cout<<n;
-    }else if(n==10){
-        cout<<9;
-    }else{
-        for(int i=11; i<=n; i++){
-            int temp = i;
-            int now = 1;
-            while(temp > 0){
-                now*= temp%10;
-                temp/=10;
-            }
-            if(now <max){
-                max=now;
-            }
+void fun(int& a, int& b, int c, int d){
+    unsigned long long b1,a1, m,i,t1=a,t2=b, t3=c, t4=d;
+    b1=t2*t4,a1=t1*t4+t2*t3;
+    m=min(a1, b1);
+    for(i=2; i<=m;i++){
+        if(a1%i ==0 && b1%i ==0){
+            a1/=i;
+            b1/=i;
+            i=1;
+            m=min(a1, b1);
         }
-        cout<<max;
+        a= a1, b=b1;
     }
+}
+int Pow(int i){
+    int t=1;
+    for(int j=1; j<=i; j++){
+        t*=10;
+    }
+    return t;
+}
+int P1(int i){
+    int t=0;
+    for(int j=1; j<=i;j++){
+        t=t*10+9;
+    }
+    return t;
+}
+int main(){
+    int a, i, a1, a2, n, flag;
+    char s1[10], s2[10], s3[10],t;
+    // while(cin>>n&&n){
+        // getchar();
+        // while(n--){
+            flag=0;
+            s1[0]=s1[0]=s3[0]=0;
+            i=0;
+            t=getchar();
+            if(t=='-'){
+                flag=1;
+            }else{
+                s1[i++]=t;
+            }
+            while(1){
+                t=getchar();
+                if(t=='\n'){
+                    s1[i]=0;
+                    goto End;
+                }
+                if(t=='.'){
+                    s1[i] = 0;
+                    break;
+                }
+                s1[i++] =t;
+            }
+            i = 0;
+            while(1){
+                t=getchar();
+                if(t=='('){
+                    s2[i] = 0;
+                    break;
+                }
+                if(t=='\n'){
+                    s2[i]=0;
+                    goto End;
+                }
+                s2[i++]=t;
+            }
+            i=0;
+            while(1){
+                t=getchar();
+                if(t==')'){
+                    s3[i]=0;
+                    getchar();
+                    goto End;
+                }
+                s3[i++] = t;
+            }
+            End:;
+            a1=0;
+            a2=1;
+            if(strlen(s1)!=0&&s1[0]!='0'){
+                sscanf(s1, "%d", &a);
+                a1=a;
+            }
+            if(strlen(s2)!=0){
+                sscanf(s2, "%d", &a);
+                fun(a1, a2, a, Pow(strlen(s2)));
+            }
+            if(strlen(s3)!=0){
+                sscanf(s3, "%d", &a);
+                fun(a1, a2, a, P1(strlen(s3))*Pow(strlen(s2)));
+            }
+            if(flag){
+                cout<<'-';
+            }
+            cout<<a1<<'/'<<a2<<endl;
+        // }
+    // }
     return 0;
 }
--- tests/javascript/test.js
+++ tests/javascript/test.js
@@ -1,59 +1,13 @@
-function partion(arr, left, right){
-  let i = left;
-  let j = right;
-  let base = arr[left];
-  while(i<j){
-    while(arr[j]>=base && i<j){
-      j--;
-    }
-    while(arr[i]<=base && i<j){
-      i++;
-    }
-    let tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-  }
-  arr[left] = arr[i];
-  arr[i] = base;
-  return i;
+function getRandomFloat(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.random() * (max - min) + min; //不含最大值，含最小值
 }
-function sort(arr, left, right){
-  if(left < right){
-    let mid = partion(arr, left, right);
-    sort(arr, left, mid-1);
-    sort(arr, mid+1, right);
-  }
-}
-
-let arr = [3,7, 0,0];
-let brr = [];
-let zero_len = 0;
-let map = {};
-let congfu = false;
+// let arr = [55.26, 56.34, 56.41, 57.13, 57.15, 57.19, 58.11, 59.63, 61.67];
+// let arr = [70.24, 71.55, 71.89, 72.01];
+let arr = [77.42, 77.44, 78.07, 78.16];
+let new_arr = [];
 arr.forEach(e=>{
-  if(e!==0){
-    if(map[e]){
-      congfu = true; 
-    }else{
-      map[e] = 1;
-    }
-    brr.push(e);
-  }else{
-    zero_len ++;
-  }
+  new_arr.push(e-getRandomFloat(10,40)/10);
 });
-// console.log(map);
-sort(brr, 0, brr.length-1);
-// console.log(brr);
-let cha = 0;
-if(brr.length>1){
-  cha = brr[brr.length-1] - brr[0]+1-brr.length;
-}
-if(zero_len>=cha && !congfu){
-  console.log("YES+"+zero_len);
-}
-else if(congfu){
-  console.log("NO+"+zero_len);
-}else{
-  console.log("NO+"+zero_len);
-}
+console.log(new_arr);
