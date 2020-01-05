fb183626df29a25e4365244235b06bde4a633b17
mannuan <1271990125@qq.com>
2020-01-05 12:52:30 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,24 @@
-console.log(location.pathname);
-if(location.pathname == "/"){
+function getQueryStringArgs() {
+    var qs = (location.search.length > 0 ? location.search.substring(1) : ""), 
+        args = {},
+        items = qs.length ? qs.split("&") : [],
+        item = null,
+        name = null,
+        value = null,
+        i = 0,
+        len = items.length;
+    for (i=0; i < len; i++){
+        item = items[i].split("=");
+        name = decodeURIComponent(item[0]);
+        value = decodeURIComponent(item[1]);
+        if (name.length) {
+            args[name] = value;
+        }
+    }
+    return args;
+}
+
+let args = getQueryStringArgs(location.href);
+if(location.pathname == "/" && JSON.stringify(args) !== "{}"){
     location.href = "//blog.mannuan.group";
 }
